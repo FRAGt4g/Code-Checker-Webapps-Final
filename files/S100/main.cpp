7d633f46fefd6f358472ddcb274222172c7da433
Miles F <90790095+FRAGt4g@users.noreply.github.com>
2024-05-08 01:40:52 +0000

    added java support bbut maybe broke c++

---
+++ files/S100/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 12+2;
+    if (a == 14) {
+        int b = 9;
+        cout << b << endl;
+    }
+    cout << a << endl;
+    return 0;
+}
